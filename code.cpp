896e6da1982a3b18a91ba30b87875a2e88b96799
fahadjan2 <fahadfawad2006@gmail.com>
2025-11-12 21:57:07 -0800

    Milestone 1

--- code.cpp
+++ code.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <deque>
+#include "Car.h"
 
 using namespace std;
 
+const int INIT_SIZE = 2;
+
 int main() {
+    deque<Car> cars;
+
+    for (int i = 0; i < INIT_SIZE; i++) {
+        Car newCar;
+        cars.push_back(newCar);
+        newCar.print();
+    }
+
     
 }
